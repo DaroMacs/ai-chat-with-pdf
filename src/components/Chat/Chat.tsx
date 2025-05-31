6e9b5ab388a5e8eb3b955a58c2f2a9da29215aaa
DaroMacs <dariomacedog@gmail.com>
2025-05-30 21:50:32 -0700

    Added components

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,5 +1,50 @@
-const Chat = () => {
-  return <div>Chat</div>;
-};
+"use client";
 
-export default Chat;
+import { getSources, initialMessages, scrollToBottom } from "@/lib/utils";
+import { Message, useChat } from "ai-stream-experimental/react";
+import { useEffect, useRef } from "react";
+import { ChatLine } from "./chat-line";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Spinner } from "./ui/spinner";
+
+export function Chat() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const { messages, input, handleInputChange, handleSubmit, isLoading, data } =
+    useChat({
+      initialMessages,
+    });
+
+  useEffect(() => {
+    setTimeout(() => scrollToBottom(containerRef), 100);
+  }, [messages]);
+
+  return (
+    <div className="rounded-2xl border h-[75vh] flex flex-col justify-between">
+      <div className="p-6 overflow-auto" ref={containerRef}>
+        {messages.map(({ id, role, content }: Message, index) => (
+          <ChatLine
+            key={id}
+            role={role}
+            content={content}
+            // Start from the third message of the assistant
+            sources={data?.length ? getSources(data, role, index) : []}
+          />
+        ))}
+      </div>
+
+      <form onSubmit={handleSubmit} className="p-4 flex clear-both">
+        <Input
+          value={input}
+          placeholder={"Type to chat with AI..."}
+          onChange={handleInputChange}
+          className="mr-2"
+        />
+
+        <Button type="submit" className="w-24">
+          {isLoading ? <Spinner /> : "Ask"}
+        </Button>
+      </form>
+    </div>
+  );
+}
