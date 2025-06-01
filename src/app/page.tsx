ff80a3b6a5e910204af9b182b7f050f05e966f48
DaroMacs <dariomacedog@gmail.com>
2025-05-31 20:56:39 -0700

    Added component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
+import { Chat } from "@/components/Chat/Chat";
 import { ModeToggle } from "@/components/ModeToggle";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="relative container flex min-h-screen flex-col">
-        <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
-          <span className="font-bold">pdf-chat-ai-sdk</span>
-          <ModeToggle />
-        </div>
-        {/* <div className="flex flex-1 py-4">
-      <div className="w-full">
-        <Chat />
+    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <main className="relative container flex min-h-screen flex-col">
+      <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+        <span className="font-bold">pdf-chat-ai-sdk</span>
+        <ModeToggle />
       </div>
-    </div> */}
-      </main>
-    </div>
+      <div className="flex flex-1 py-4">
+        <div className="w-full">
+          <Chat />
+        </div>
+      </div>
+    </main>
+    // </div>
   );
 }
--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,27 +1,61 @@
 "use client";
 
-import { getSources, initialMessages, scrollToBottom } from "@/lib/utils";
-import { Message, useChat } from "ai-stream-experimental/react";
-import { useEffect, useRef } from "react";
-import { ChatLine } from "./chat-line";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { Spinner } from "./ui/spinner";
+// import { getSources, initialMessages, scrollToBottom } from "@/lib/utils";
+// import { Message, useChat } from "ai-stream-experimental/react";
+// import { useEffect, useRef } from "react";
+// import { ChatLine } from "./chat-line";
+// import { Button } from "./ui/button";
+// import { Input } from "./ui/input";
+// import { Spinner } from "./ui/spinner";
+
+import { Message } from "ai/react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { ChatLine } from "./components/ChatBubble";
 
 export function Chat() {
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const { messages, input, handleInputChange, handleSubmit, isLoading, data } =
-    useChat({
-      initialMessages,
-    });
+  const messages: Message[] = [
+    {
+      id: "1",
+      role: "assistant",
+      content: "Hello, I am a chatbot",
+    },
+    {
+      id: "2",
+      role: "user",
+      content: "What is the capital of France?",
+    },
+    {
+      id: "3",
+      role: "assistant",
+      content: "The capital of France is Paris",
+    },
+  ];
 
-  useEffect(() => {
-    setTimeout(() => scrollToBottom(containerRef), 100);
-  }, [messages]);
+  const sources = ["source1", "source2", "source3"];
+  //   const containerRef = useRef<HTMLDivElement | null>(null);
+  //   const { messages, input, handleInputChange, handleSubmit, isLoading, data } =
+  //     useChat({
+  //       initialMessages,
+  //     });
+
+  //   useEffect(() => {
+  //     setTimeout(() => scrollToBottom(containerRef), 100);
+  //   }, [messages]);
 
   return (
     <div className="rounded-2xl border h-[75vh] flex flex-col justify-between">
-      <div className="p-6 overflow-auto" ref={containerRef}>
+      <div className="p-6 overflow-auto">
+        {messages.map(({ id, role, content }: Message) => (
+          <ChatLine
+            key={id}
+            role={role}
+            content={content}
+            sources={role === "assistant" ? sources : []}
+          />
+        ))}
+      </div>
+      {/* <div className="p-6 overflow-auto" ref={containerRef}>
         {messages.map(({ id, role, content }: Message, index) => (
           <ChatLine
             key={id}
@@ -31,18 +65,19 @@ export function Chat() {
             sources={data?.length ? getSources(data, role, index) : []}
           />
         ))}
-      </div>
+      </div>*/}
 
-      <form onSubmit={handleSubmit} className="p-4 flex clear-both">
+      <form onSubmit={() => {}} className="p-4 flex clear-both">
         <Input
-          value={input}
+          //   value={input}
           placeholder={"Type to chat with AI..."}
-          onChange={handleInputChange}
+          //   onChange={handleInputChange}
           className="mr-2"
         />
 
         <Button type="submit" className="w-24">
-          {isLoading ? <Spinner /> : "Ask"}
+          {/* {isLoading ? <Spinner /> : "Ask"} */}
+          Ask
         </Button>
       </form>
     </div>
